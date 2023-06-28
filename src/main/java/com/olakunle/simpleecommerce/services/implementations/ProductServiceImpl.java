a02bcf956df801369fcfd73436dbc1df0701113d
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-06-28 10:19:31 +0100

    closes #9, Swagger add at http://localhost:8080/swagger-ui.index.html

--- src/main/java/com/olakunle/simpleecommerce/services/implementations/ProductServiceImpl.java
+++ src/main/java/com/olakunle/simpleecommerce/services/implementations/ProductServiceImpl.java
@@ -3,9 +3,11 @@ package com.olakunle.simpleecommerce.services.implementations;
 import com.olakunle.simpleecommerce.dto.requests.ProductRequest;
 import com.olakunle.simpleecommerce.dto.responses.ProductResponse;
 import com.olakunle.simpleecommerce.services.ProductService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductServiceImpl implements ProductService {
     @Override
     public List<ProductResponse> getAllProducts() {
