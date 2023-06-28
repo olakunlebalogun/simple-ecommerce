7ce3af8a8242fbf1e0f8181407da6799ba9f9594
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-06-28 10:07:10 +0100

    added exception to the method to redirect to swagger

--- src/main/java/com/olakunle/simpleecommerce/routes/HomeController.java
+++ src/main/java/com/olakunle/simpleecommerce/routes/HomeController.java
@@ -4,11 +4,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public void gotoSwagger(HttpServletResponse response) {
+    public void gotoSwagger(HttpServletResponse response) throws IOException {
         response.sendRedirect("/swagger-ui.html");
     }
+
 }
